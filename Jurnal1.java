6964c45d36c18519dcff28f7c8ffbbb34afc32d2
aqsa <aqsaksatriawan@gmail.com>
2023-09-27 16:57:55 +0700

    Initial Commit

---
+++ Jurnal1.java
@@ -0,0 +1,13 @@
+public class Jurnal1 {
+    public static void main(String[] args) {
+        String Nama, Kelas, NIM;
+        Nama = "Muhammad Fayyaz Aqsa Ksatriawan";
+        Kelas = "D3IF-47-03";
+        NIM = "607062300119";
+
+        System.out.println("Nama\t:" +Nama);
+        System.out.println("Kelas\t:" +Kelas);
+        System.out.println("NIM\t:" +NIM);
+    }
+    
+}
